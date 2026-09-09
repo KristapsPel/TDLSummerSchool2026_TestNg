8a2893f67b4930f8579a508f986889800dfffefd
KristapsPel <kristaps.pelcbergs@testdevlab.com>
2026-09-09 13:07:24 +0300

    PT 3

--- src/test/java/add/addTest.java
+++ src/test/java/add/addTest.java
@@ -1,56 +1,85 @@
 package add;
 
-//Add TestNG dependency and plugin Done
-//Create add folder inside project/src/test/java Done
-//Create addTest class Done
-//Inside class addTest Create 4 test method with @Test annotation Done
-//In each metode use different values for add method from Calculatore.java class Done
-//Check if result is correct and print in console result (If test pass or failed) Done
+//Create dataProviderMethod with @DataProvider annotation that returns Object[][] Done
+//Provide 3 variable (a, b and expected result) add all values from previous created tests Done
+//Add dataProvider data to test Done
+//Use data from dataProvider in test Done
+//Add testName and description Done
+//Print in terminal testDescription Done
+//Run the same 4 tests that we did run on previous practical task using just one test method Done
+
 
 import calculator.Calculator;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.lang.reflect.Method;
 
 public class addTest {
 
     Calculator calculator = new Calculator();
 
-    @Test
-    public void testAddPositiveInt(){
-        int result = calculator.add(2, 3);
-        if(result==-1){
-            System.out.println("Test Pass!");
+    @DataProvider(name = "addTestData")
+    public Object[][] createTestDataForAdd(){
+        return new Object[][]{
+                {2 , 3, -1},
+                {-2, -3, -5},
+                {2, -3, -1},
+                {2, 0, 2}
+        };
+    }
+
+    @Test(dataProvider = "addTestData",
+            testName = "Add Test case",
+            description = "Testing add methode for Calculator class")
+    public void testAddCalculator(int a, int b, int result, Method method){
+        String description = method.getAnnotation(Test.class).description();
+        System.out.println("Description:"+description);
+        int actualResult=calculator.add(a,b);
+        if(actualResult==result){
+            System.out.println("PASS");
         } else {
-            System.out.println("Test Failed!");
+            System.out.println("Actual result:"+actualResult+" but expected:"+result);
         }
     }
 
-    @Test
-    public void testAddNegativeInt(){
-        int result = calculator.add(-2, -3);
-        if(result==-5){
-            System.out.println("Test Pass!");
-        } else {
-            System.out.println("Test Failed!");
-        }
-    }
-
-    @Test
-    public void testAddNegativeAndPositiveInt(){
-        int result = calculator.add(2, -3);
-        if(result==-1){
-            System.out.println("Test Pass!");
-        } else {
-            System.out.println("Test Failed!");
-        }
-    }
-
-    @Test
-    public void testAddWithZero(){
-        int result = calculator.add(2, 0);
-        if(result==2){
-            System.out.println("Test Pass!");
-        } else {
-            System.out.println("Test Failed!");
-        }
-    }
+//    @Test
+//    public void testAddPositiveInt(){
+//        int result = calculator.add(2, 3);
+//        if(result==-1){
+//            System.out.println("Test Pass!");
+//        } else {
+//            System.out.println("Test Failed!");
+//        }
+//    }
+//
+//    @Test
+//    public void testAddNegativeInt(){
+//        int result = calculator.add(-2, -3);
+//        if(result==-5){
+//            System.out.println("Test Pass!");
+//        } else {
+//            System.out.println("Test Failed!");
+//        }
+//    }
+//
+//    @Test
+//    public void testAddNegativeAndPositiveInt(){
+//        int result = calculator.add(2, -3);
+//        if(result==-1){
+//            System.out.println("Test Pass!");
+//        } else {
+//            System.out.println("Test Failed!");
+//        }
+//    }
+//
+//    @Test
+//    public void testAddWithZero(){
+//        int result = calculator.add(2, 0);
+//        if(result==2){
+//            System.out.println("Test Pass!");
+//        } else {
+//            System.out.println("Test Failed!");
+//        }
+//    }
 }
